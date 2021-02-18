e0e1e57752f4c5267460027060897b895cd2a83d
Mohammad Nour Rezek <Eng.M_Nour@outlook.com>
2021-02-18 08:52:30 +0200

    add restaurant data model

---
+++ FoodSilver.Data/Models/CuisineType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSilver.Data.Models
+{
+    // to limit the number of cuisines we add an enum
+    public enum CuisineType
+    {
+        // the first represent a restaurant that have no cuisine
+        None,
+        Italian,
+        Indian,
+        Arabian
+    }
+}
---
+++ FoodSilver.Data/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSilver.Data.Models
+{
+    // add public modifier to use the class outside of the project
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
