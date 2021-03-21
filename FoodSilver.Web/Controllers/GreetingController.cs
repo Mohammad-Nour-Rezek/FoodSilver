3519de0859a74577f027cd1208b48941abe5d4ae
Mohammad Nour Rezek <Eng.M_Nour@outlook.com>
2021-03-22 00:12:07 +0200

    show message from config xml file

---
+++ FoodSilver.Web/Controllers/GreetingController.cs
@@ -0,0 +1,22 @@
+﻿using FoodSilver.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodSilver.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var viewModel = new GreetingMessage();
+            viewModel.Message = ConfigurationManager.AppSettings["message"];
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FoodSilver.Web/Models/GreetingMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodSilver.Web.Models
+{
+    public class GreetingMessage
+    {
+        public string Message { get; set; }
+    }
+}
