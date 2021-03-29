b2e752bc836c5e2546b7fa56eb7e5e9b2100a5e2
Mohammad Nour Rezek <Eng.M_Nour@outlook.com>
2021-03-29 07:38:02 +0300

    add validation and info

--- FoodSilver.Web/App_Start/BundleConfig.cs
+++ FoodSilver.Web/App_Start/BundleConfig.cs
@@ -8,10 +8,13 @@ namespace FoodSilver.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // for debug and production
+            // we can add here jQueryValidation and bootstarp in one bundle
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                // *: means any word after it like 'unobutrusive'
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
